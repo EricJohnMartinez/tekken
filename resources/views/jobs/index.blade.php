0cef1e926ba2e0cb6d1ed34e42b4d585964306ec
EricJohnMartinez <ericjohn121709@gmail.com>
2023-05-09 03:09:28 +0800

    update

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -284,7 +284,7 @@
                     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.17/dist/sweetalert2.min.js"></script>
                     <link rel="stylesheet"
                         href="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.17/dist/sweetalert2.min.css" />
-
+                    
                     <script>
                         function deleteJob(jobId) {
                             Swal.fire({
